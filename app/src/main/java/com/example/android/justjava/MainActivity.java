6e87b1c8cca238a390f94986ccffaf566b185c37
Ninth Factor <ninthfactor@gmail.com>
2015-07-16 22:37:49 -0400

    added more features. checkboxes for whipping and chocolate and order summary

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.android.justjava;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -10,7 +11,7 @@ import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
 
     int quantity = 2;
@@ -21,16 +22,61 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param quantity is the number of cups of coffee ordered
+     */
+    private int calculatePrice(int quantity, int pricePerCoffee) {
+        int price = quantity * pricePerCoffee;
+        return price ;
+    }
+
     /**
      * This method is called when the order button is clicked.
      */
+
+    // Method to create a summary text when order button is clicked
+
+    private String createOrderSummary(int price, String toppingString, int quantity){
+        return "Name: Varun\n"+toppingString+"Quantity:"+quantity+"\n"+"Total:"+price+  "\nThank You!";
+
+    }
+
+
+    // Create topping text details to add to final order
+
+    private String getToppingText(View view){
+        CheckBox topping1 = (CheckBox)findViewById(R.id.topping1);
+        CheckBox topping2 = (CheckBox)findViewById(R.id.topping2);
+        String toppingText = "";
+        if (topping1.isChecked())
+                 toppingText = toppingText + "Whipping Cream: True\n";
+        else
+                 toppingText = toppingText + "Whipping Cream : False\n";
+
+        if (topping2.isChecked())
+            toppingText = toppingText + "Chocolate: True\n";
+        else
+            toppingText = toppingText + "Chocolate : False\n";
+
+        return  toppingText;
+
+    }
+
+
+
+
     public void submitOrder(View view) {
 
      //   display(quantity);
      //   displayPrice(quantity * 5);
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price  + "\nThank You!";
-        displayMessage(priceMessage);
+
+        int price = calculatePrice(quantity, 5 );
+        String toppingText = getToppingText(view);
+        String orderSummaryTextView = createOrderSummary(price, toppingText, quantity);
+        displayMessage(orderSummaryTextView);
     }
 
     // call this method when the plus button is hit
@@ -38,7 +84,7 @@ public class MainActivity extends ActionBarActivity {
     public void increment(View view){
 
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
         //displayPrice(quantity * 5);
 
     }
@@ -48,7 +94,7 @@ public class MainActivity extends ActionBarActivity {
     public void decrement(View view){
 
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
         //displayPrice(quantity * 5);
 
     }
@@ -56,7 +102,7 @@ public class MainActivity extends ActionBarActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
@@ -66,7 +112,7 @@ public class MainActivity extends ActionBarActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
@@ -74,7 +120,7 @@ public class MainActivity extends ActionBarActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
